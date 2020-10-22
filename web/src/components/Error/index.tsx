5114652552c42032958af3937605402bc6a050e1
kynefuk <atsuminamihira@gmail.com>
2020-10-22 21:34:37 +0900

    fix Error component

--- web/src/components/Error/index.tsx
+++ web/src/components/Error/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useRootContext } from "../../context/index";
-import { IconButton, Collapse } from "@material-ui/core";
+import { IconButton, Collapse, Container } from "@material-ui/core";
 import CloseIcon from "@material-ui/icons/Close";
 import { Alert, AlertTitle } from "@material-ui/lab";
 import { ErrorActionType } from "../../action/type";
@@ -19,26 +19,28 @@ export const Error: React.FC = () => {
 
   return (
     <>
-      {error && (
-        <Collapse in={open}>
-          <Alert
-            severity="error"
-            action={
-              <IconButton
-                aria-label="close"
-                color="inherit"
-                size="small"
-                onClick={handleOnClose}
-              >
-                <CloseIcon fontSize="inherit" />
-              </IconButton>
-            }
-          >
-            <AlertTitle>Error</AlertTitle>
-            {error}
-          </Alert>
-        </Collapse>
-      )}
+      <Container component="main" maxWidth="xs">
+        {error && (
+          <Collapse in={open}>
+            <Alert
+              severity="error"
+              action={
+                <IconButton
+                  aria-label="close"
+                  color="inherit"
+                  size="small"
+                  onClick={handleOnClose}
+                >
+                  <CloseIcon fontSize="inherit" />
+                </IconButton>
+              }
+            >
+              <AlertTitle>Error</AlertTitle>
+              {error}
+            </Alert>
+          </Collapse>
+        )}
+      </Container>
     </>
   );
 };
