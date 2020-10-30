767323453d4a15a06dc177122d1e5eaaa465bc3e
kynefuk <atsuminamihira@gmail.com>
2020-10-30 23:48:18 +0900

    fix serverless and db

--- server/apps/configs/db.py
+++ server/apps/configs/db.py
@@ -4,9 +4,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-MYSQL_DATABASE_URL = os.environ.get(
+DB_URL = os.environ.get(
     "MYSQL_DATABASE_URL", "mysql+pymysql://root:root@db/blog"
 )
+DB_USER = os.environ.get("DB_USER", "root")
+DB_PASSWORD = os.environ.get("DB_PASSWORD", "password")
+DB_NAME = os.environ.get("DB_NAME", "blog")
+
+MYSQL_DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_URL}/{DB_NAME}"
 
 engine = create_engine(MYSQL_DATABASE_URL)
 
