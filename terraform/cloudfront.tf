9b7afd56b0639547f9d45fd4cba2039ddd04d301
kynefuk <atsuminamihira@gmail.com>
2020-11-07 22:02:12 +0900

    fix frontend bucket name

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -37,8 +37,8 @@ resource "aws_cloudfront_distribution" "frontend_distribution" {
 
     viewer_protocol_policy = "https-only"
     min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
+    default_ttl            = 0
+    max_ttl                = 0
   }
 
   price_class = "PriceClass_200"
